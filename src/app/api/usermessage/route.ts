0224727440ebb25a1e2cea81559994a1369df415
ArtemValik <meromen666@gmail.com>
2026-07-30 16:01:27 +0500

    апдейт Contact

--- src/app/api/usermessage/route.ts
+++ src/app/api/usermessage/route.ts
@@ -34,7 +34,7 @@ export async function POST(request: NextRequest) {
     } catch(error){
 
         if (error instanceof Error) {
-            return NextResponse.json({ error: error.message }, { status: 500 });
+            return NextResponse.json({ error: 'Внутренняя ошибка сервера' }, { status: 500 });
         }
 
         return NextResponse.json({ error: 'Внутренняя ошибка сервера' }, { status: 500 });
